90ea0a73931fb9c301636b659519460446d21c43
152242Gitile <keziah.gitile@strathmorre.edu>
2024-06-29 12:06:00 +0300

    Second commit

--- public/index.php
+++ public/index.php
@@ -1,55 +1,336 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Complete responsiveness Fitness and health Website Design</title>
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+<!-- Swiper css link -->
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
 
-define('LARAVEL_START', microtime(true));
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
+<!--  Custom CSS file link -->
+      <link rel="stylesheet"  href="CSS/style.css">
+     
+     <!-- fontawesome cdn file link -->
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+      integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
+       crossorigin="anonymous"/>
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+</head>
+<body>
+<header class="header">
+    <a href="#" class="logo">Nutri<span>Plan</span></a>
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
+    <nav class="navbar">
+        <a href="index.php">home</a>
+        <a href="about.php">about</a>
+        <a href="#">services</a>
+        <a href="#">news</a>
+        <a href="#">contact</a>
+    </nav>
 
-require __DIR__.'/../vendor/autoload.php';
+    <div class=" icons">
+        <a href="#" class="btn">Join us</a>
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </div>
+</header>
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
+<!--home section starts-->
+    <section class="home">
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+        <div class=" swiper home-slider">
 
-$kernel = $app->make(Kernel::class);
+            <div class="swiper-wrapper">
 
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
+                <div class="swiper-slide box" style="background: linear-gradient(rgba(0,0,0,.3),rgba(0,0,0,.3)),
+                 url('images/fitness-nutrition-pic-1.png');">
+                    <div class="content">
+                          <h3>Join nutriplan today</h3>
+                            <p>Follow your plan not your mood.</p>
+                             <div class="button">
+                                <a href="#" class="btn btn-1">get started</a>
+                                <a href="#" class="btn">download</a>
+                            </div>
+                    </div>
+                    
 
-$kernel->terminate($request, $response);
+                </div>
+
+                <div class="swiper-slide box" style="background: linear-gradient(rgba(0,0,0,.3),rgba(0,0,0,.3)),
+                 url('images/fitness image2.jpg');">
+                        <div class="content">
+                                <h3>Health is wealth</h3>
+                                        <p>Follow your plan not your mood.</p>
+                                            <div class="button">
+                                                <a href="index.php" class="btn btn-1">get started</a>
+                                                <a href="index.php" class="btn">download</a>
+                                             </div>
+                         </div>
+                 </div>
+            </div>
+
+         </div>
+        
+
+
+    </section>
+
+
+
+
+<!--home section ends-->
+
+<!-- feature section starts-->
+
+<section class="swiper feature">
+    <h1 class="heading">feartured <span>Classes</span></h1>
+
+    <div class="feature-slider">
+
+        <div class="swiper-wrapper">
+            <div class="swiper-slide box">
+                <div class="image">
+                    <img src="images/fitness-nutrition-pic-1.png" alt="">
+                </div>
+                <div class="content">
+                    <div class="intensity">low</div>
+                    <h3>Yoga</h3>
+                </div>
+            </div>
+
+            <div class="swiper-slide box">
+                <div class="image">
+                    <img src="images/fitness image.jpeg" alt="">
+                </div>
+                <div class="content">
+                    <div class="intensity">high</div>
+                    <h3>Cardio</h3>
+                </div>
+            </div>
+
+            <div class="swiper-slide box">
+                <div class="image">
+                    <img src="images/fitness image2.jpg" alt="">
+                </div>
+                <div class="content">
+                    <div class="intensity">medium</div>
+                    <h3>Zumba</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- feature section ends-->
+
+<!-- schedule scetion starts-->
+
+<section class="schedule">
+    <h1 class="heading"><span>our </span>schedule</h1>
+    <div class="box-container">
+        <div class="box">
+            <div class="content">
+                <h3>Yoga</h3>
+                <p>There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..</p>
+            </div>
+            <div class="icons">
+                <span><i class="far fa-clock"></i>3:30PM</span>
+                <span><i class="far fa-calendar-alt"></i>June 16,2024</span>
+            </div>
+            <a href="#" class="btn">Join class</a>
+        </div>
+
+        <div class="box">
+            <img src="images/gym-diet-cover-1.jpg" alt="">
+        </div>
+
+        <div class="box">
+            <div class="content">
+                <h3>Cardio</h3>
+                <p>There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..</p>
+            </div>
+            <div class="icons">
+                <span><i class="far fa-clock"></i>3:30PM</span>
+                <span><i class="far fa-calendar-alt"></i>June 16,2024</span>
+            </div>
+            <a href="#" class="btn">Join class</a>
+        </div>
+
+        <div class="box">
+            <img src="images/gym-diet-cover-2.jpg" alt="">
+        </div>
+
+        <div class="box">
+            <div class="content">
+                <h3>Zumba</h3>
+                <p>There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..</p>
+            </div>
+            <div class="icons">
+                <span><i class="far fa-clock"></i>3:30PM</span>
+                <span><i class="far fa-calendar-alt"></i>June 16,2024</span>
+            </div>
+            <a href="#" class="btn">Join class</a>
+        </div>
+
+        <div class="box">
+            <img src="images/exercise-nutrition.jpg" alt="">
+        </div>
+    </div>
+</section>
+
+<!-- schedule scetion ends-->
+
+<!----------------trainer section starts------------------>
+
+<section class="trainers">
+
+<h1 class="heading">Expert <span>trainers</span></h1>
+
+<div class="swiper trainer-slider">
+
+    <div class="swiper-wrapper">
+
+        <div class="swiper-slide box">
+            <div class="image">
+                <img src="images/trainer-1.jpg" alt="">
+            </div>
+            <div class="name">
+                <h1>Alvaro Pennington</h1>
+                <p>gym trainer</p>
+            </div>
+        </div>
+   
+        <div class="swiper-slide box">
+            <div class="image">
+                <img src="images/trainer-2.jpg" alt="">
+            </div>
+            <div class="name">
+                <h1>Dorothy Snow</h1>
+                <p>gym trainer</p>
+            </div>
+        </div>
+
+        <div class="swiper-slide box">
+            <div class="image">
+                <img src="images/trainer-5.jpg" alt="">
+            </div>
+            <div class="name">
+                <h1>Edwin Cooley</h1>
+                <p>gym trainer</p>
+            </div>
+        </div>
+
+        <div class="swiper-slide box">
+            <div class="image">
+                <img src="images/trainer-3.jpg" alt="">
+            </div>
+            <div class="name">
+                <h1>Karin Padilla</h1>
+                <p>gym trainer</p>
+            </div>
+        </div>
+
+        <div class="swiper-slide box">
+            <div class="image">
+                <img src="images/trainer-6.jpg" alt="">
+            </div>
+            <div class="name">
+                <h1>Antony House</h1>
+                <p>gym trainer</p>
+            </div>
+        </div>
+
+        <div class="swiper-slide box">
+            <div class="image">
+                <img src="images/trainer-4.jpg" alt="">
+            </div>
+            <div class="name">
+                <h1>Rosetta Hurley</h1>
+                <p>gym trainer</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+</section>
+
+<!----------------trainer section ends------------------>
+
+<!-------------------footer section starts-------------------->
+
+<section class="footer">
+    <div class="box-container">
+
+        <div class="box">
+            <h1>about</h1>
+            <div class="text">
+                <p>"There is no one who loves pain itself,
+                     who seeks after it and wants to have it,
+                      simply because it is pain..."</p>
+            </div>
+            <div class="icon">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-snapchat"></i></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <h1>contact info</h1>
+            <div class="icon">
+                <a href="#"><i class="fas fa-map-marker-alt"></i>Nairobi, Kenya</a>
+                <a href="#"><i class="fas fa-phone-alt"></i>+254 715 250 642</a>
+                <a href="#"><i class="fas fa-envelope"></i>nutriplan@gmail.com</a>
+                </div>
+            </div>
+            </div>
+           
+        </div>
+
+        <div class="box">
+            <h1>quick links</h1>
+           <div class="icons">
+            <a href="#">home</a>
+            <a href="#">about</a>
+            <a href="#">services</a>
+            <a href="#">news</a>
+            <a href="#">contact</a>
+           </div>
+            
+        
+        </div>
+            
+           
+    </div>
+<div class="credit">created by<span> Keziah and Karuga</span>|all rights reserved</div>
+</section>
+
+
+
+
+
+
+<!-------------------footer section ends-------------------->
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<!-- custom JS file link -->
+<script src="script.JS"></script>
+
+</body>
+
+
+
+
+</html>
